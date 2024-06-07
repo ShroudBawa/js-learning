a04e6b4a5dbc2429255cdc737b35585e12129f25
Amitabh Tripathi <75723270+ShroudBawa@users.noreply.github.com>
2024-06-07 07:14:19 +0000

    configuration change

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountID = 12443
+let accountEmail = "amitabh@google.com"
+var accountPassword = "37707"
+accountCity = "Lucknow"
+let accountState;
+
+// account ID = 2 // not allowed
+
+accountEmail = "test@gmail.com"
+accountPassword = "12345"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+
+console.table([accountEmail, accountID, accountPassword, accountState])
