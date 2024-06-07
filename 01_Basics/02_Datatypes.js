9d6e7ce4f3c71787e11507fe7727696633ac8745
Amitabh Tripathi <75723270+ShroudBawa@users.noreply.github.com>
2024-06-07 10:01:11 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS Code in the newer version
+
+// alert ( 3+3 )  // we're using nodeJs , not browser 
+
+console.log(3+3)
+// code readability should be high
+console.log("amitabh")
+
+let name = "Amitabh"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to pwer of 43
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof null) // output will be coming as Object
+console.log(typeof undefined) // output will be coming as undefined
