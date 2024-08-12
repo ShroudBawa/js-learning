08be2e548259389bd25028ede8378c5f1a5ab5d6
Amitabh Tripathi <75723270+ShroudBawa@users.noreply.github.com>
2024-08-12 19:38:57 +0000

    conversion 2 dt

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,3 +25,34 @@ let score = "me"
 
  let stringNumber = String ( someNumber)
  console.log(typeof stringNumber);
+
+
+ // ************************* Operations *************************
+
+ let value = 3
+ let negValue = -value
+ // console.log(negValue);
+
+
+// console.log(2+2);
+
+
+let str1 = "hello"
+let str2 = " hello2"
+
+let str3 = str1 + str2
+console.log (str3);
+
+console.log ("1" + 2);
+console.log (1+2+"3");
+
+console.log(+true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 
+
+
+let gameCounter = 200
+gameCounter++;
+console.log(gameCounter);
